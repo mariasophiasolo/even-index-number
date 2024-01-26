eccac87d9deb81398f829ee52f76e367af8d41ff
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-26 19:39:11 +0800

    working

---
+++ even_index_number.py
@@ -0,0 +1,9 @@
+# printing characters from string that are present at an even index number
+
+# ask user to input stringg
+greeting = input('Type a greeting: ')
+print ("Original greeting: ", greeting)
+
+# initialize a variable where you can store the length of the string
+# repeat the characters in the string at even indices
+# then print the character at the even index
