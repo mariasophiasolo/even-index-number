f078aae7a5c2e1e695c7da29b1116c2e9b836320
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-26 20:00:58 +0800

    trial and error

--- even_index_number.py
+++ even_index_number.py
@@ -1,9 +1,13 @@
 # printing characters from string that are present at an even index number
 
 # ask user to input stringg
-greeting = input('Type a greeting: ')
-print ("Original greeting: ", greeting)
+endearment = input('Type a greeting: ')
+print ("Original greeting: ", endearment)
 
 # initialize a variable where you can store the length of the string
+length =  len(endearment)
 # repeat the characters in the string at even indices
 # then print the character at the even index
+print ("Printing only even index chars")
+for i in range (0, length - 1, 2):
+    print ("index[", i, "]", endearment [i])
