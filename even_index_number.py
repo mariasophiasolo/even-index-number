8d920e7994773cb4e24c465030424e9d34d1625c
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-26 20:05:37 +0800

    all is working final

--- even_index_number.py
+++ even_index_number.py
@@ -7,7 +7,7 @@ print ("Original greeting: ", endearment)
 # initialize a variable where you can store the length of the string
 length =  len(endearment)
 # repeat the characters in the string at even indices
-# then print the character at the even index
 print ("Printing only even index chars")
 for i in range (0, length - 1, 2):
+# then print the character at the even index
     print ("index[", i, "]", endearment [i])
